e7892d79acad7680e1b4083112f73938544bef92
DepressionSpaghetti <102665381+DepressionSpaghetti@users.noreply.github.com>
2023-10-08 15:16:03 +0200

    Add files via upload

--- XML-kolekcije/Form1.Designer.cs
+++ XML-kolekcije/Form1.Designer.cs
@@ -34,7 +34,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.txtIspis = new System.Windows.Forms.TextBox();
+            this.txtIspis = new System.Windows.Forms.RichTextBox();
             this.btnObrada = new System.Windows.Forms.Button();
             this.btnUnos = new System.Windows.Forms.Button();
             this.btnIspis = new System.Windows.Forms.Button();
@@ -42,94 +42,108 @@
             // 
             // txtModel
             // 
-            this.txtModel.Location = new System.Drawing.Point(303, 70);
+            this.txtModel.Location = new System.Drawing.Point(152, 36);
+            this.txtModel.Margin = new System.Windows.Forms.Padding(2);
             this.txtModel.Name = "txtModel";
-            this.txtModel.Size = new System.Drawing.Size(208, 31);
+            this.txtModel.Size = new System.Drawing.Size(106, 20);
             this.txtModel.TabIndex = 0;
             // 
             // txtGod
             // 
-            this.txtGod.Location = new System.Drawing.Point(303, 162);
+            this.txtGod.Location = new System.Drawing.Point(152, 84);
+            this.txtGod.Margin = new System.Windows.Forms.Padding(2);
             this.txtGod.Name = "txtGod";
-            this.txtGod.Size = new System.Drawing.Size(208, 31);
+            this.txtGod.Size = new System.Drawing.Size(106, 20);
             this.txtGod.TabIndex = 1;
             this.txtGod.TextChanged += new System.EventHandler(this.txtGod_TextChanged);
             // 
             // txtKotac
             // 
-            this.txtKotac.Location = new System.Drawing.Point(303, 270);
+            this.txtKotac.Location = new System.Drawing.Point(152, 140);
+            this.txtKotac.Margin = new System.Windows.Forms.Padding(2);
             this.txtKotac.Name = "txtKotac";
-            this.txtKotac.Size = new System.Drawing.Size(208, 31);
+            this.txtKotac.Size = new System.Drawing.Size(106, 20);
             this.txtKotac.TabIndex = 2;
             this.txtKotac.TextChanged += new System.EventHandler(this.txtKotac_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(53, 76);
+            this.label1.Location = new System.Drawing.Point(26, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 25);
+            this.label1.Size = new System.Drawing.Size(36, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Model";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(53, 165);
+            this.label2.Location = new System.Drawing.Point(26, 86);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(198, 25);
+            this.label2.Size = new System.Drawing.Size(98, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Godina proizvodnje";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(53, 273);
+            this.label3.Location = new System.Drawing.Point(26, 142);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(120, 25);
+            this.label3.Size = new System.Drawing.Size(61, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Broj kotaca";
             // 
             // txtIspis
             // 
-            this.txtIspis.Location = new System.Drawing.Point(130, 553);
+            this.txtIspis.Location = new System.Drawing.Point(65, 288);
+            this.txtIspis.Margin = new System.Windows.Forms.Padding(2);
             this.txtIspis.Multiline = true;
             this.txtIspis.Name = "txtIspis";
-            this.txtIspis.Size = new System.Drawing.Size(536, 299);
+            this.txtIspis.Size = new System.Drawing.Size(270, 157);
             this.txtIspis.TabIndex = 6;
+            this.txtIspis.Click += new System.EventHandler(this.txtIspisClicked);
             // 
             // btnObrada
             // 
-            this.btnObrada.Location = new System.Drawing.Point(130, 366);
+            this.btnObrada.Location = new System.Drawing.Point(65, 190);
+            this.btnObrada.Margin = new System.Windows.Forms.Padding(2);
             this.btnObrada.Name = "btnObrada";
-            this.btnObrada.Size = new System.Drawing.Size(130, 121);
+            this.btnObrada.Size = new System.Drawing.Size(65, 63);
             this.btnObrada.TabIndex = 7;
             this.btnObrada.Text = "Obradi";
             this.btnObrada.UseVisualStyleBackColor = true;
+            this.btnObrada.Click += new System.EventHandler(this.btnObrada_Click);
             // 
             // btnUnos
             // 
-            this.btnUnos.Location = new System.Drawing.Point(333, 366);
+            this.btnUnos.Location = new System.Drawing.Point(166, 190);
+            this.btnUnos.Margin = new System.Windows.Forms.Padding(2);
             this.btnUnos.Name = "btnUnos";
-            this.btnUnos.Size = new System.Drawing.Size(130, 121);
+            this.btnUnos.Size = new System.Drawing.Size(65, 63);
             this.btnUnos.TabIndex = 8;
             this.btnUnos.Text = "Unesi";
             this.btnUnos.UseVisualStyleBackColor = true;
+            this.btnUnos.Click += new System.EventHandler(this.btnUnos_Click);
             // 
             // btnIspis
             // 
-            this.btnIspis.Location = new System.Drawing.Point(536, 366);
+            this.btnIspis.Location = new System.Drawing.Point(268, 190);
+            this.btnIspis.Margin = new System.Windows.Forms.Padding(2);
             this.btnIspis.Name = "btnIspis";
-            this.btnIspis.Size = new System.Drawing.Size(130, 121);
+            this.btnIspis.Size = new System.Drawing.Size(65, 63);
             this.btnIspis.TabIndex = 9;
             this.btnIspis.Text = "Ispisi";
             this.btnIspis.UseVisualStyleBackColor = true;
+            this.btnIspis.Click += new System.EventHandler(this.btnIspis_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 894);
+            this.ClientSize = new System.Drawing.Size(392, 465);
             this.Controls.Add(this.btnIspis);
             this.Controls.Add(this.btnUnos);
             this.Controls.Add(this.btnObrada);
@@ -140,8 +154,10 @@
             this.Controls.Add(this.txtKotac);
             this.Controls.Add(this.txtGod);
             this.Controls.Add(this.txtModel);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Vozila";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -155,7 +171,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtIspis;
+        private System.Windows.Forms.RichTextBox txtIspis;
         private System.Windows.Forms.Button btnObrada;
         private System.Windows.Forms.Button btnUnos;
         private System.Windows.Forms.Button btnIspis;
